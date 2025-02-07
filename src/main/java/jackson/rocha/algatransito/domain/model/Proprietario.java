e47d1d9f1c8ab01df12f2dcf9ab9ef26e511cf24
jacksonrocha <jackson_lbr@hotmail.com>
2025-02-06 21:34:11 -0300

    Mapeando o tabela Proprietario com o Lombok

--- src/main/java/jackson/rocha/algatransito/domain/model/Proprietario.java
+++ src/main/java/jackson/rocha/algatransito/domain/model/Proprietario.java
@@ -1,14 +1,26 @@
 package jackson.rocha.algatransito.domain.model;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+//@Table(name = "proprietario")
 public class Proprietario {
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
     private String email;
+
+    @Column(name = "fone")
     private String telefone;
+
 }
