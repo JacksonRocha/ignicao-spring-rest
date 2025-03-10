ac0f417873d33a86c91ec50bb7d32f830275f6bb
jacksonrocha <jackson_lbr@hotmail.com>
2025-03-10 13:48:26 -0300

    Propriedades do banco

--- src/main/java/jackson/rocha/algatransito/api/controller/ProprietarioController.java
+++ src/main/java/jackson/rocha/algatransito/api/controller/ProprietarioController.java
@@ -1,30 +1,22 @@
 package jackson.rocha.algatransito.api.controller;
 
 import jackson.rocha.algatransito.domain.model.Proprietario;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class ProprietarioController {
 
+    @PersistenceContext
+    private EntityManager manager;
+
     @GetMapping("/proprietarios")
     public List<Proprietario> listar() {
-        var proprietario1 = new Proprietario();
-        proprietario1.setId(1L);
-        proprietario1.setNome("Jackson Rocha");
-        proprietario1.setEmail("jackson@hotmail.com");
-        proprietario1.setTelefone("999999999");
-
-        var proprietario2 = new Proprietario();
-        proprietario2.setId(2L);
-        proprietario2.setNome("Maria Silva");
-        proprietario2.setEmail("Maria@gmail.com");
-        proprietario2.setTelefone("888888888");
-
-        return Arrays.asList(proprietario1, proprietario2);
-
+        return manager.createQuery("from Proprietario", Proprietario.class)
+                .getResultList();
     }
 }
